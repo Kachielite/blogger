88398c91b6899a2570fee5a58d658378aa54ca9b
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-03-09 23:29:05 +0100

    added repository

---
+++ src/main/java/com/derrick/blogger/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.derrick.blogger.repository;
+
+import com.derrick.blogger.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByAuthority(String authority);
+}
---
+++ src/main/java/com/derrick/blogger/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.derrick.blogger.repository;
+
+import com.derrick.blogger.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
